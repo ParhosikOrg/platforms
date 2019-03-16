aad4fa84ace57bb30d7f45ba99adf70989df3209
Sikandar Mahmood <Sikandar Mahmood@DESKTOP-IELO7FO>
2019-03-17 02:25:19 +0500

    committed after last login.

--- platform/src/main/java/com/sap/platform/model/User.java
+++ platform/src/main/java/com/sap/platform/model/User.java
@@ -2,58 +2,75 @@ package com.sap.platform.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="users")
+@Table(name="user")
 public class User {
 
 	@Id
 	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
-	
-	@Column(name="first_name")
-	private String firstName;
-	
-	@Column(name="middle_name")
-	private String middleName;
-	
-	@Column(name="last_name")
-	private String lastName;
 	
 	@Column(name="username")
 	private String userName;
 	
-	@Column(name="pass")
+	@Column(name="password")
 	private String password;
 
+	@Column(name="type")
+	private int type;
+
 	public User() {
-		super();
+		//
 	}
 	
-	public User(int id, String firstName, String middleName, String lastName, String userName, String password) {
-		super();
+	public User(int id, String userName, String password, int type) {
 		this.id = id;
-		this.firstName = firstName;
-		this.middleName = middleName;
-		this.lastName = lastName;
 		this.userName = userName;
 		this.password = password;
+		this.type = type;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", userName=" + userName + ", password=" + password + ", type=" + type + "]";
 	}
 	
 }
